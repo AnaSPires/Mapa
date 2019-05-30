732ee7f1bd34e30750b032d03eccdfd97495e0a4
ArianeBarros <42739320+ArianeBarros@users.noreply.github.com>
2019-05-30 10:37:03 -0300

    sasas

---
+++ Projeto base - apCaminhosMarte/apCaminhosMarte/Cidade.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+class Cidade : IComparable<Cidade>  
+{
+    const int tamanhoMatricula = 5;
+    const int tamanhoNome = 30;
+    const int tamanhoSalario = 10;
+    
+    const int inicioMatricula = 0;
+    const int inicioNome = inicioMatricula + tamanhoMatricula;
+    const int inicioSalario = inicioNome + tamanhoNome;
+
+    public int matricula;
+    public string nome;
+    public double salario;
+    public Cidade()
+    {
+      matricula = 0;
+      nome = "";
+      salario = 0;
+    }
+    public Cidade(string linha)
+    {
+       matricula = int.Parse(linha.Substring(inicioMattricula, tamanhoMatricula));
+       nome = linha.Substring(inicioNome, tamanhoNome);
+       salario = double.Parse(linha.Substring(inicioSalario, tamanhoSalario));
+    }
+    public int CompareTo(Cidade outroFunc)
+    {
+       return matricula - outroFunc.matricula;
+    }
+}
+
